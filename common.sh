c8e36ab5008835d1fd9681fc72621fb23795779d
nodiscc <nodiscc@gmail.com>
2015-06-17 13:07:21 +0200

    update functions

--- common.sh
+++ common.sh
@@ -2,3 +2,8 @@
 _getTimestamp() {
     date +"%Y-%m-%d_%H%M%S"
 }
+
+_CreateDB() {
+	QUERY="create database $1; create user $1@localhost identified by \"$2\"; grant all on $1.* to $1@localhost;"
+	mysql -u root -p -e "$QUERY"
+}
