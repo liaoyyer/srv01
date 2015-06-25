8df98d055c9d2d4e2d5d563c290a3d272b2bc7f0
nodiscc <nodiscc@gmail.com>
2015-06-25 18:40:29 +0200

    update scripts/yourls.sh

--- scripts/yourls.sh
+++ scripts/yourls.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-_ChangePassword() {
+changepassword() {
 	#edit /var/www/yourls/user/config.php, array $yourls_user_passwords
+	echo "Not yet implemented."
+}
+
+_install() {
+		echo "Not yet implemented."
+}
+
+_uninstall() {
+		echo "Not yet implemented."
+}
+
+_status() {
 	echo "Not yet implemented."
 }
