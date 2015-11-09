65d1167b9bc4cebeddb6601f8db56029497e4613
nodiscc <nodiscc@gmail.com>
2015-11-09 15:53:41 +0100

    restore alltube role, WIP

---
+++ roles/webapp-alltube/files/var_www_alltube_config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * PHP web interface for youtube-dl (http://rg3.github.com/youtube-dl/)
+ * Config file
+ *
+ * PHP Version 5.3.10
+ *
+ * @category Youtube-dl
+ * @package  Youtubedl
+ * @author   Pierre Rudloff <contact@rudloff.pro>
+ * @license  GNU General Public License http://www.gnu.org/licenses/gpl.html
+ * @link     http://rudloff.pro
+ * */
+define('YOUTUBE_DL', '/usr/bin/youtube-dl');
+define('PYTHON', '/usr/bin/python');
+define('PARAMS', '--no-playlist --no-warnings');
+define('CONVERT', true);
+define('MAINTENANCE', false);
+define('DISABLED', false);
+?>
